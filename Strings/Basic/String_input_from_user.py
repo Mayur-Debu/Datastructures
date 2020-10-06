b1da86e3ee5da94c7928098d41e3a60ce318e30c
Mayur Manohar Patil <iammayur.515@gmail.com>
2020-10-05 20:42:11 -0700

    Initial commit

---
+++ Strings/Basic/String_input_from_user.py
@@ -0,0 +1,8 @@
+'''
+Write a basic program to take input(String) from user and just print it.
+'''
+
+
+if __name__ == '__main__':
+    userInputString=input('Enter a string: ')
+    print(f'The user input string is {userInputString}')

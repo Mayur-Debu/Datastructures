fbc4ee8c48e40fa8769164b12c11670ade2c10ef
Mayur Manohar Patil <iammayur.515@gmail.com>
2020-10-08 10:09:15 -0700

    Initial commit

---
+++ Strings/Intermediate/Split_binary_tree.py
@@ -0,0 +1,25 @@
+'''
+Write a program to split the binary tree into substrings with equal 0's and 1's.
+'''
+
+
+def binarySubstring(string):
+    zeroCount = 0
+    oneCount = 0
+    subStringCount = 0
+
+    for i in range(len(string)):
+        if (string[i] == "0"):
+            zeroCount += 1
+        elif (string[i] == "1"):
+            oneCount += 1
+
+        if zeroCount == oneCount:
+            subStringCount += 1
+
+    return subStringCount
+
+
+if __name__ == '__main__':
+    binaryString = input('Enter a binary string with 0\'s and 1\'s: ')
+    print(binarySubstring(binaryString))

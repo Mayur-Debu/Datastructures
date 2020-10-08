032feaf474c202abe97dbea85e61344a40c4d943
Mayur Manohar Patil <iammayur.515@gmail.com>
2020-10-08 09:26:45 -0700

    Initial commit

--- Strings/Intermediate/permutations_of_given_string.py
+++ Strings/Intermediate/permutations_of_given_string.py
@@ -2,15 +2,17 @@
 Write a programa to print all permutations of a given string.
 '''
 
-def getPermutation(s,l,r):
-    if l ==r :
+
+def getPermutation(s, l, r):
+    if l == r:
         print("".join(s))
     else:
-        for i in range(l,r):
-            s[l],s[i]=s[i],s[l]
-            getPermutation(s,l+1,r)
-            s[l],s[i]=s[i],s[l]                #bactrack to the previous step.
+        for i in range(l, r):
+            s[l], s[i] = s[i], s[l]
+            getPermutation(s, l + 1, r)
+            s[l], s[i] = s[i], s[l]  # bactrack to the previous step.
+
 
 if __name__ == '__main__':
-    string=list(input('Enter the string: '))
-    getPermutation(string,0,len(string))
+    string = list(input('Enter the string: '))
+    getPermutation(string, 0, len(string))

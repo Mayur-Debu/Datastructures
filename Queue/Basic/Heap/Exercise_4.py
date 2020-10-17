60a80fb14152d88764712f98b7778a0181ce0ab0
Mayur Manohar Patil <iammayur.515@gmail.com>
2020-10-17 09:48:39 -0700

    Initial commit

---
+++ Queue/Basic/Heap/Exercise_4.py
@@ -0,0 +1,15 @@
+"""
+Write a program to print the maximum of sub array of size k.
+"""
+
+
+def maxOfSubarrayK(numbers,n,k):
+    for i in range(n-k+1):
+        print(max(numbers[i:i+k]),end=' ')
+
+
+if __name__ == '__main__':
+    numbers=list(map(int,input('Enter the elements in array: ').strip(' ').split(' ')))
+    n=len(numbers)
+    k=int(input('Enter k vaue: '))
+    maxOfSubarrayK(numbers,n,k)

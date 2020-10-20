34006be5f944e9dd17d06c0bcea84bb5bfdd29dd
Mayur Manohar Patil <iammayur.515@gmail.com>
2020-10-20 16:51:47 -0700

    Initial commit

--- Linked List/Basic/Exercise_9.py
+++ Linked List/Basic/Exercise_9.py
@@ -1,44 +1,48 @@
 """
 Implementation of the circular doubly linked list.
 """
+
+
 class Node:
-    def __init__(self,data):
-        self.data=data
-        self.next=None
-        self.previous=None
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.previous = None
+
 
 class CircularDoublyLinkedList:
     def __init__(self):
-        self.head=Node(None)
-        self.tail=Node(None)
-        self.head.next=self.tail
-        self.tail.next=self.head
+        self.head = Node(None)
+        self.tail = Node(None)
+        self.head.next = self.tail
+        self.tail.next = self.head
 
-    def insertAtEnd(self,data):
-        node=Node(data)
+    def insertAtEnd(self, data):
+        node = Node(data)
         if self.head.data is None:
-            self.head=self.tail=node
-            self.head.next=self.head
-            self.head.previous=None
+            self.head = self.tail = node
+            self.head.next = self.head
+            self.head.previous = None
         else:
-            self.tail.next=node
-            node.previous=self.tail
-            self.tail=node
-            self.tail.next=self.head
+            self.tail.next = node
+            node.previous = self.tail
+            self.tail = node
+            self.tail.next = self.head
 
     def display(self):
         if self.head is None:
             print('The linked list is empty')
         else:
-            iterator=self.head
-            print(iterator.data,end=' --> ')
-            while iterator.next!=self.head:
-                iterator=iterator.next
-                print(iterator.data,end=' --> ')
+            iterator = self.head
+            print(iterator.data, end=' --> ')
+            while iterator.next != self.head:
+                iterator = iterator.next
+                print(iterator.data, end=' --> ')
             print(None)
 
+
 if __name__ == '__main__':
-    cdll=CircularDoublyLinkedList()
+    cdll = CircularDoublyLinkedList()
     cdll.insertAtEnd(5)
     cdll.insertAtEnd(6)
     cdll.insertAtEnd(5)

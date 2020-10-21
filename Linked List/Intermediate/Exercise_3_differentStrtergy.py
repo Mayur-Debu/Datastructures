ffd9bad624897548e91b7fe32cefd477258a29f9
Mayur Manohar Patil <iammayur.515@gmail.com>
2020-10-21 07:25:50 -0700

    Initial commit

--- Linked List/Intermediate/Exercise_3_differentStrtergy.py
+++ Linked List/Intermediate/Exercise_3_differentStrtergy.py
@@ -1,6 +1,4 @@
-"""
-Write  a program to reverse a linked list.
-"""
+"""Write a program to reverse a linked list"""
 
 
 class Node:
@@ -29,22 +27,6 @@ class LinkedList:
         for data in dataList:
             self.insertAtEnd(data)
 
-    def insertAtFront(self, data):
-        node = Node(data)
-        if self.head is None:
-            self.head = node
-            node.next = None
-        else:
-            node.next = self.head
-            self.head = node
-
-    def reverseIterative(self):
-        iterative = self.head
-        self.head = None
-        while iterative:
-            self.insertAtFront(iterative.data)
-            iterative = iterative.next
-
     def display(self):
         iterator = self.head
         while iterator:
@@ -52,10 +34,19 @@ class LinkedList:
             iterator = iterator.next
         print(None)
 
+    def reverseLinkedList(self):
+        iterator = self.head
+        string = []
+        while iterator:
+            string.append(iterator.data)
+            iterator = iterator.next
+        self.head = None
+        self.insertList(string[::-1])
+
 
 if __name__ == '__main__':
     ll = LinkedList()
-    ll.insertList([1, 2, 3, 4])
+    ll.insertList([1, 2, 3, 4, 5])
     ll.display()
-    ll.reverseIterative()
+    ll.reverseLinkedList()
     ll.display()

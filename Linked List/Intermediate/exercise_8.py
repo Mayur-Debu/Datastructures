e1d0843d334df8f476f4c0c75333cc1a1f556638
Mayur Manohar Patil <iammayur.515@gmail.com>
2020-10-21 17:03:11 -0700

    Initial commit

---
+++ Linked List/Intermediate/exercise_8.py
@@ -0,0 +1,61 @@
+"""
+Write a program to remove duplicates from the sorted linked list.
+"""
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insertAtEnd(self, data):
+        '''This function inserts the new node at the end of the linked list.'''
+        if self.head is None:
+            self.head = Node(data)
+            return
+
+        iterator = self.head
+        while iterator.next:
+            iterator = iterator.next
+        iterator.next = Node(data)
+
+    def insertList(self, data_list):
+        '''This function inserts the list of nodes in the linked list.'''
+        self.head = None
+        string = []
+        for data in data_list:
+            string.append(data)
+            self.insertAtEnd(data)
+        print('Original Linked list:', end=' ')
+        self.print()
+        print('Altered Linked list:', end=' ')
+        self.removeDuplicates(set(string))
+
+    def removeDuplicates(self, string):
+        self.head = None
+        for data in string:
+            self.insertAtEnd(data)
+
+    def print(self):
+        '''This function prints the linked list starting from the head till the tail.'''
+        if self.head is None:
+            print('The linked list is empty')
+            return
+
+        iterator = self.head
+
+        while iterator:
+            print(iterator.data, end=' -->')
+            iterator = iterator.next
+        print(None)
+
+
+if __name__ == '__main__':
+    ll = LinkedList()
+    ll.insertList([1, 2, 2, 3, 4, 5, 5, 5, 5, 5])
+    ll.print()

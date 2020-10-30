44b0df5362cf63ad85a4b05fdae40f4ca9776bb8
Mayur Manohar Patil <iammayur.515@gmail.com>
2020-10-30 09:44:36 -0700

    Initial commit

---
+++ Linked List/Intermediate/Exercise_29.py
@@ -0,0 +1,6 @@
+"""
+Can we reverse a linked list less than o(n) time complexity?
+--> I don't think it's possible as, the linked list is said to move in one direction only, it is going to take 0(N) time.
+"""
+
+
